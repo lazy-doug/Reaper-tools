0d2a9b2095833eda2f8bb880bb0c3d5f92db22f5
lazy-doug <shischel@gmail.com>
2025-07-06 14:46:42 +0300

    RMAP 1.1 - Add table record editing

--- Relative Midi Action Pairs/Relative Midi Action Pairs Main.lua
+++ Relative Midi Action Pairs/Relative Midi Action Pairs Main.lua
@@ -1,7 +1,6 @@
+-- @noindex
 -- Package: Relative Midi Action Pairs
 -- Fires different actions for each relative CC direction
--- @noindex
-
 
 
 local EXTSECTION = "Relative_CC_Toolkit"
@@ -57,11 +56,22 @@ local function config_gui()
 
   local configs = CONFIGS
   local new_entry = { cc = 0, chan = 1, inc = 0, dec = 0 }
+  local editing_index = nil
+  local edit_entry = { cc = 0, chan = 1, inc = '', dec = '' }
 
   local function get_action_name(cmd)
     local numeric_cmd = reaper.NamedCommandLookup(cmd) or cmd
     local name = reaper.kbd_getTextFromCmd(numeric_cmd, 0)
     return (name ~= "") and name or "(Unknown)"
+  end
+
+
+  local function check_duplicate(entry, index)
+    for j, cfg in ipairs(configs) do
+      if j ~= index and cfg.cc == entry.cc and cfg.chan == entry.chan then
+        return true
+      end
+    end
   end
 
   local function loop()
@@ -82,24 +92,67 @@ local function config_gui()
         reaper.ImGui_TableHeadersRow(ctx)
 
         for i = 1, #configs do
+          local editing = (editing_index == i)
           local cfg = configs[i]
           reaper.ImGui_TableNextRow(ctx)
-          reaper.ImGui_TableSetColumnIndex(ctx, 0)
-          reaper.ImGui_Text(ctx, tostring(cfg.chan))
-          reaper.ImGui_TableSetColumnIndex(ctx, 1)
-          reaper.ImGui_Text(ctx, tostring(cfg.cc))
-          reaper.ImGui_TableSetColumnIndex(ctx, 2)
-          reaper.ImGui_Text(ctx, tostring(cfg.dec))
-          reaper.ImGui_TableSetColumnIndex(ctx, 3)
-          reaper.ImGui_Text(ctx, get_action_name(cfg.dec))
-          reaper.ImGui_TableSetColumnIndex(ctx, 4)
-          reaper.ImGui_Text(ctx, tostring(cfg.inc))
-          reaper.ImGui_TableSetColumnIndex(ctx, 5)
-          reaper.ImGui_Text(ctx, get_action_name(cfg.inc))
-          reaper.ImGui_TableSetColumnIndex(ctx, 6)
-          if reaper.ImGui_Button(ctx, "Delete##" .. i) then
-            table.remove(configs, i)
-            save_config(configs)
+
+          if editing then
+            reaper.ImGui_TableSetColumnIndex(ctx, 0)
+            reaper.ImGui_SetNextItemWidth(ctx, -1)
+            _, edit_entry.chan = reaper.ImGui_InputInt(ctx, '##edit_chan' .. i, edit_entry.chan)
+            reaper.ImGui_TableSetColumnIndex(ctx, 1)
+            reaper.ImGui_SetNextItemWidth(ctx, -1)
+            _, edit_entry.cc = reaper.ImGui_InputInt(ctx, '##edit_cc' .. i, edit_entry.cc)
+            reaper.ImGui_TableSetColumnIndex(ctx, 2)
+            reaper.ImGui_SetNextItemWidth(ctx, -1)
+            _, edit_entry.dec = reaper.ImGui_InputText(ctx, '##edit_dec' .. i, edit_entry.dec, 256)
+            reaper.ImGui_TableSetColumnIndex(ctx, 3)
+            reaper.ImGui_Text(ctx, get_action_name(edit_entry.dec))
+            reaper.ImGui_TableSetColumnIndex(ctx, 4)
+            reaper.ImGui_SetNextItemWidth(ctx, -1)
+            _, edit_entry.inc = reaper.ImGui_InputText(ctx, '##edit_inc' .. i, edit_entry.inc, 256)
+            reaper.ImGui_TableSetColumnIndex(ctx, 5)
+            reaper.ImGui_Text(ctx, get_action_name(edit_entry.inc))
+            reaper.ImGui_TableSetColumnIndex(ctx, 6)
+            if reaper.ImGui_Button(ctx, 'Save##' .. i) then
+              local duplicate = check_duplicate(edit_entry, i)
+              if not duplicate then
+                configs[i] = { cc = edit_entry.cc, chan = edit_entry.chan, inc = edit_entry.inc, dec = edit_entry.dec }
+                save_config(configs)
+                editing_index = nil
+              else
+                reaper.ShowMessageBox('Conflict: Duplicate Channel+CC.', 'Update Error', 0)
+              end
+            end
+            reaper.ImGui_SameLine(ctx)
+            if reaper.ImGui_Button(ctx, 'Cancel##' .. i) then editing_index = nil end
+          else
+            reaper.ImGui_TableSetColumnIndex(ctx, 0)
+            reaper.ImGui_Text(ctx, tostring(cfg.chan))
+            reaper.ImGui_TableSetColumnIndex(ctx, 1)
+            reaper.ImGui_Text(ctx, tostring(cfg.cc))
+            reaper.ImGui_TableSetColumnIndex(ctx, 2)
+            reaper.ImGui_Text(ctx, tostring(cfg.dec))
+            reaper.ImGui_TableSetColumnIndex(ctx, 3)
+            reaper.ImGui_Text(ctx, get_action_name(cfg.dec))
+            reaper.ImGui_TableSetColumnIndex(ctx, 4)
+            reaper.ImGui_Text(ctx, tostring(cfg.inc))
+            reaper.ImGui_TableSetColumnIndex(ctx, 5)
+            reaper.ImGui_Text(ctx, get_action_name(cfg.inc))
+            reaper.ImGui_TableSetColumnIndex(ctx, 6)
+            if reaper.ImGui_Button(ctx, 'Edit##' .. i) then
+              editing_index = i
+              edit_entry.cc = cfg.cc
+              edit_entry.chan = cfg.chan
+              edit_entry.inc = tostring(cfg.inc)
+              edit_entry.dec = tostring(cfg.dec)
+            end
+            reaper.ImGui_SameLine(ctx)
+            if reaper.ImGui_Button(ctx, 'Delete##' .. i) then
+              table.remove(configs, i)
+              save_config(configs)
+              break
+            end
           end
         end
 
@@ -122,13 +175,7 @@ local function config_gui()
         reaper.ImGui_Text(ctx, get_action_name(new_entry.inc))
         reaper.ImGui_TableSetColumnIndex(ctx, 6)
         if reaper.ImGui_Button(ctx, 'Add##new') then
-          local duplicate = false
-          for _, cfg in ipairs(configs) do
-            if cfg.cc == new_entry.cc and cfg.chan == new_entry.chan then
-              duplicate = true
-              break
-            end
-          end
+          local duplicate = check_duplicate(new_entry)
           if not duplicate then
             table.insert(configs, {
               cc = new_entry.cc,
